b7e9db200ef4574fb8acd089c10c06ff16ced13b
JetTsang <JetTsang8086@163.com>
2022-11-27 21:54:53 +0800

    feat:完成tab页构建

--- src/views/middle/middle.tsx
+++ src/views/middle/middle.tsx
@@ -3,62 +3,126 @@ import cartoonImg from '../../assets/img/cartoon.jpg'
 import foodImg from '../../assets/img/food.jpg'
 import movieImg from '../../assets/img/movie.png'
 import lifeImg from '../../assets/img/life.jpg'
-import { useState ,FC} from 'react'
+import logoImg from '../../assets/img/logo.png'
+import { useState, FC, useRef, useEffect } from 'react'
 import styles from './middle.module.scss'
 
 const TabsList = [
     {
-        key:'cartoon',
-        name:'动画',
-        img:cartoonImg
+        key: 'cartoon',
+        name: '动画',
+        img: cartoonImg
     },
     {
-        key:'food',
-        name:'美食',
-        img:foodImg
+        key: 'food',
+        name: '美食',
+        img: foodImg
     },
     {
-        key:'movie',
-        name:'电影',
-        img:movieImg
+        key: 'movie',
+        name: '电影',
+        img: movieImg
     },
     {
-        key:'life',
-        name:'生活',
-        img:lifeImg
+        key: 'life',
+        name: '生活',
+        img: lifeImg
     },
 ]
 
 
-const Middle:FC = ()=> {
-   const [activeTab,setActiveTab]  = useState<string>('cartoon')
-   console.log(activeTab)
- return (
-   <section style={{'marginTop':'8px'}} >
-       {/* tabs */}
-       <div className={styles['tabs-content']}>
-           <ul>
-               {
-                   TabsList.map(i=>(
-                       <li key={i.key} onClick={()=>{setActiveTab(i.key)}}>
-                        <span>{i.name}</span>
-                        <span className={`${styles.line} ${activeTab==i.key ? styles.lineVisible:''}`}></span>
-                        </li>
-                   ))
-               }
-           </ul>
-       </div>
-       {/* comment */}
-       {
-            TabsList.map(i=>(
-                <section className={styles.content} key={i.key}>
-                    <h2>{i.name}</h2>
-                    <img src={i.img}></img>
-                </section>
-            ))
-       }
-       
-   </section>
- )
+
+
+const Middle: FC = () => {
+    const [activeTab, setActiveTab] = useState<string>('cartoon')
+    const [isFixed,setisFixed]  = useState<boolean>(false)
+
+
+    const middleRef = useRef<HTMLElement>(null)
+
+    const handleClickTabs = (key:string)=>{
+        setActiveTab(key)
+        contentIntoView(key)
+    }
+    // 点击tab ，实现content跳转到视口
+    const contentIntoView = (key:string)=>{
+        if(middleRef.current){
+            const  content = middleRef.current.querySelector(`[data-id=${key}]`)
+            content?.scrollIntoView({behavior:'smooth'})
+        }
+    }
+
+    // 向下滚动的时候 ，实现吸顶
+    const onWindowScroll = ()=>{
+
+        if(middleRef.current){
+            const {top} = middleRef.current.getBoundingClientRect() 
+            setisFixed(top<=0)
+            //  滚动到某个位置，实现 tabs亮
+            const contentList:Element[] = []
+            TabsList.forEach(i=>{
+                const  content:Element | null = (middleRef as any).current.querySelector(`[data-id=${i.key}]`)
+                if(content){
+                    contentList.push(content)
+                }
+            })
+            Array.prototype.forEach.call(contentList,(i)=>{
+                if(i){
+                    const {top} = i.getBoundingClientRect()
+                    if(top>0 && top <= 56){
+                        const id = i.getAttribute('data-id')
+                        setActiveTab(id)
+                    }
+                }
+            })
+        }
+    }
+
+
+
+    // 组件创建之后去监听
+    useEffect(()=>{
+        window.addEventListener('scroll',onWindowScroll)
+        return ()=>{
+            window.removeEventListener('scroll',onWindowScroll)
+        }
+    },[])
+
+
+
+
+    return (
+        <section style={{ 'marginTop': '8px'}} ref={middleRef} className={styles.middle}>
+            {/* tabs */}
+            <div className={`${styles['tabs-content']} ${isFixed ? styles.fixed : ''}`}>
+                <ul>
+                    {
+                        TabsList.map(i => (
+                            <li key={i.key} onClick={() => { handleClickTabs(i.key) }}>
+                                <span>{i.name}</span>
+                                <span className={`${styles.line} ${activeTab == i.key ? styles.lineVisible : ''}`}></span>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+            {/* content */}
+            {
+                TabsList.map(i => (
+                    <section className={styles.content} key={i.key} data-id={i.key}>
+                        <h2>{i.name}</h2>
+                        <img src={i.img}></img>
+                    </section>
+                ))
+            }
+            {/* 底下吸底的按钮 */}
+            <div className={`${styles.bottomBtn} ${isFixed ? styles.visible : ''}`} >
+                <img src={logoImg}></img>
+                <a href="https://www.bilibili.com/" target="_blank">
+                    <button>App 内打开</button>
+                </a>
+            </div>
+        </section>
+    )
 }
 export default Middle
